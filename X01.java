ef519b8c7438d1aa1ac2d114b01bc6a2e2ff1e38
JosefMarpaung <josefchristian31@gmail.com>
2024-10-09 16:36:04 +0700

    Pair-14

--- X01.java
+++ X01.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class X01 {
+public class X01{
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -35,4 +35,3 @@ public class X01 {
         return String.format("%." + digits + "f", value);
     }
 }
-
